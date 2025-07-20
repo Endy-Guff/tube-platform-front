6e076cabdaf241020e5b55e4cd1dd66fe25a0398
Endy-Guff <andreymaranchak@yandex.ru>
2025-07-20 23:19:16 +0300

    feat(video-card): анимация на framer-motion

--- src/components/ui/video-card/index.tsx
+++ src/components/ui/video-card/index.tsx
@@ -4,6 +4,7 @@ import { PUBLIC_ROUTES } from '@config'
 import { IVideo } from '@services'
 import cn from 'clsx'
 import { parseISO } from 'date-fns'
+import * as m from 'framer-motion/m'
 import { User } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -21,62 +22,64 @@ export const VideoCard: FC<IVideo> = ({
   channel,
   viewsCount,
   createdAt
-}) => {
-  return (
-    <div className={styles.card}>
-      <div className={styles.preview}>
-        <Link href={PUBLIC_ROUTES.VIDEO(publicId)}>
-          <Image
-            className={styles['preview-img']}
-            src={thumbnailUrl}
-            alt={title}
-            width={VIDEO_CARD_SIZES.width}
-            height={VIDEO_CARD_SIZES.height}
-          />
-        </Link>
-        <Link
-          className={styles['channel-link']}
-          href={PUBLIC_ROUTES.CHANNEL(channel.slug)}
-        >
-          <Image
-            className={styles['channel-img']}
-            src={channel.avatarUrl}
-            alt={''} // todo: дописать имя канала channel.name
-            width={30}
-            height={30}
-          />
-        </Link>
-      </div>
-
-      <Box
-        className={styles.info}
-        alignItems='center'
-        justifyContent='space-between'
+}) => (
+  <m.div
+    className={styles.card}
+    whileHover={{ scale: 1.05, y: -5 }}
+    transition={{ type: 'spring', stiffness: 500, damping: 30 }}
+  >
+    <div className={styles.preview}>
+      <Link href={PUBLIC_ROUTES.VIDEO(publicId)}>
+        <Image
+          className={styles['preview-img']}
+          src={thumbnailUrl}
+          alt={title}
+          width={VIDEO_CARD_SIZES.width}
+          height={VIDEO_CARD_SIZES.height}
+        />
+      </Link>
+      <Link
+        className={styles['channel-link']}
+        href={PUBLIC_ROUTES.CHANNEL(channel.slug)}
       >
-        <Box alignItems='center'>
-          <User
-            color={COLORS.primary}
-            size={12}
-          />
-          <Typography.DESCRIPTION className={styles['views-count']}>
-            {getFormatViews(viewsCount)}
-          </Typography.DESCRIPTION>
-        </Box>
-        <div className={styles.date}>
-          <Typography.DESCRIPTION>
-            {getFormatRelativeDate(parseISO(createdAt))}
-          </Typography.DESCRIPTION>
-        </div>
-      </Box>
-
-      <div className={cn(styles.title, 'line-clamp-2')}>
-        <Link href={PUBLIC_ROUTES.VIDEO(publicId)}>{title}</Link>
-      </div>
-
-      <div className={styles.channel}>
-        <Link href={PUBLIC_ROUTES.CHANNEL(channel.slug)}>channel</Link>
-        {/*  // todo: дописать имя канала channel.name */}
-      </div>
+        <Image
+          className={styles['channel-img']}
+          src={channel.avatarUrl}
+          alt={''} // todo: дописать имя канала channel.name
+          width={30}
+          height={30}
+        />
+      </Link>
     </div>
-  )
-}
+
+    <Box
+      className={styles.info}
+      alignItems='center'
+      justifyContent='space-between'
+    >
+      <Box alignItems='center'>
+        <User
+          color={COLORS.primary}
+          size={12}
+        />
+        <Typography.DESCRIPTION className={styles['views-count']}>
+          {getFormatViews(viewsCount)}
+        </Typography.DESCRIPTION>
+      </Box>
+      <div className={styles.date}>
+        <Typography.DESCRIPTION>
+          {getFormatRelativeDate(parseISO(createdAt))}
+        </Typography.DESCRIPTION>
+      </div>
+    </Box>
+
+    <div className={cn(styles.title, 'line-clamp-2')}>
+      <Link href={PUBLIC_ROUTES.VIDEO(publicId)}>{title}</Link>
+    </div>
+
+    <div className={styles.channel}>
+      <Link href={PUBLIC_ROUTES.CHANNEL(channel.slug)}>channel</Link>
+      {/*  // todo: дописать имя канала channel.name */}
+    </div>
+  </m.div>
+)
--- src/providers/Providers.tsx
+++ src/providers/Providers.tsx
@@ -1,10 +1,15 @@
 'use client'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { LazyMotion, domAnimation } from 'framer-motion'
 import { JSX, PropsWithChildren } from 'react'
 
 const client = new QueryClient()
 
 export function Providers({ children }: PropsWithChildren): JSX.Element {
-  return <QueryClientProvider client={client}>{children}</QueryClientProvider>
+  return (
+    <QueryClientProvider client={client}>
+      <LazyMotion features={domAnimation}>{children}</LazyMotion>
+    </QueryClientProvider>
+  )
 }
